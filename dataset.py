ba31b17e47f4db2de36e950a5e9ac94dc152fb74
gracejxq <gracexjin@gmail.com>
2024-01-08 17:34:50 -0500

    update gitignore

--- dataset.py
+++ dataset.py
@@ -4,16 +4,24 @@ import torch
 
 # filepaths for raw data
 hpi = "datasets/hpi.csv"
-ten_yrt = "datasets/10yr_treasury.csv"
-wages = "datasets/wages_monthly.csv"
-unemployment = "datasets/unemployment.csv"
-supply = "datasets/supply_monthly.csv"
-# supply = "datasets/supply_quarterly.csv"
+ten_yrt = "datasets/10yr_treasury.csv" # henry
+unemployment = "datasets/unemployment.csv" # grace
+wages = "datasets/wages_monthly.csv" # chris
+# wages = "datasets/wages_quarterly.csv"
+supply = "datasets/supply.csv" # daniel
+
+# rates of data collection for input types
+daily = {ten_yrt}
+if wages == "datasets/wages_monthly.csv":
+    monthly = {unemployment, wages}
+    quarterly = {supply}
+else:
+    monthly = {unemployment}
+    quarterly = {wages, supply}
 
 # save path for final dataset
-save_path = "datasets/dataset"
-toggle = "_supply_monthly.csv" if supply == "datasets/supply_monthly.csv" else "_supply_quarterly.csv"
-save_path += toggle
+toggle = "_supply_monthly.csv" if wages == "datasets/wages_monthly.csv" else "_wages_quarterly.csv"
+save_path = "datasets/dataset" + toggle
 
 def splitDataset(x, y):
     x_train = 
@@ -25,8 +33,11 @@ def splitDataset(x, y):
     return 
     print("Dataset split into training, validation, and testing sets")
 
-# Function to construct a dataset with inputs from 10 year treasury rates, wages, unemployment
-# rates, and housing supply & outputs the HPA index; daily increments
+def hpaCalculator(year1, year2):
+    return (year2 - year1) / year1 * 100
+
+# Function to construct a dataset with inputs from 10 year treasury rates, unemployment rates,
+# wages, and housing supply & outputs the HPA index; daily increments
 def constructDataset():
     input = 
     output = 
